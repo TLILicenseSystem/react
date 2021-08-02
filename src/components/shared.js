13a46865167e5f9f17a6f4299bce9ced137a8f24
Pavidayas <86999654+Pavidayas@users.noreply.github.com>
2021-08-02 15:35:46 +0700

    Add files via upload

---
+++ src/components/shared.js
@@ -0,0 +1,5 @@
+export {InputWithLabel,InputTimeWithLabel,InputWithLabelID} from "./InputWithLabel/InputWithLabel";
+export {Container} from "./Container/Container";
+export {Wrapper} from "./Wrapper/Wrapper";
+export {Spinner} from "./Spinner/Spinner";
+export {Popup} from "./Popup/Popup";
