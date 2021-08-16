c09051f5c7cacc68bd99121ee4ebc14e6c07c1fb
Pavidayas <86999654+Pavidayas@users.noreply.github.com>
2021-08-16 11:22:51 +0700

    Add files via upload

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -2,9 +2,26 @@ import styled from "styled-components";
 import {colors} from "../../themes/style";
 
 export const Wrapper = styled.div`
-  width: ${({width}) => width ? width : '1000px'};
-  background-color: #ffffff;
-  border: 1px solid ${colors.BORDER};
-  box-shadow: 0 0 10px ${colors.BORDER};
-  padding: 15px;
+ // width: ${({width}) => width ? width : '800px'};
+  background-color:${colors.BG};
+  border-radius: 0px;
+  //border: 1px solid ${colors.BORDER};
+  //box-shadow: 0 0 10px ${colors.BORDER};
+  padding: 20px;
+  width:100%;
+  padding: 50px;
+  display: flex;
+  justify-content: flex-end; 
+`;
+export const WrapperHead = styled.div`
+ // width: ${({width}) => width ? width : '800px'};
+  background-color:${colors.BG};
+  border-radius: 0px;
+  //border: 1px solid ${colors.BORDER};
+  //box-shadow: 0 0 10px ${colors.BORDER};
+  padding: 20px;
+  width:100%;
+  padding: 50px;
+  display: flex;
+  justify-content: flex-end; 
 `;
