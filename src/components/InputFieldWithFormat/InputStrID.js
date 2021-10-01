a4e944d28afde26c3aed8a5c703c07384b372baf
AwaneeMook <awanee.mook@gmail.com>
2021-10-01 08:55:04 +0700

    update input strID

--- src/components/InputFieldWithFormat/InputStrID.js
+++ src/components/InputFieldWithFormat/InputStrID.js
@@ -2,42 +2,52 @@ import React, { useState } from "react";
 import { Input, FormFeedback, FormGroup } from "reactstrap";
 import styles from "./InputWithLabel.module.css";
 import PropTypes from "prop-types";
+import InputMask from "react-input-mask";
 
 export const InputStrID = ({ label, disabled, value, width, onChange }) => {
   const [error, setError] = useState(false);
 
   function autoTab(e) {
-    // if (e.target.value.length === 10) {
-    // } else if (e.target.value.length === 8) {
-    // } else if (e.target.value.length === 5) {
-    // } else
-
-    if (e.target.value.replaceAll("-", "").length === 3) {
+    e.target.value = e.target.value.replaceAll("-", "");
+    if (e.target.value.length === 3) {
       if (/^([P][0-9][A-Z])$/.test(e.target.value)) {
-        var pattern = new String("_-_-_"); // กำหนดรูปแบบในนี้
-        var pattern_ex = new String("-"); // กำหนดสัญลักษณ์หรือเครื่องหมายที่ใช้แบ่งในนี้
-        var returnText = new String("");
-        var obj_l = e.target.value.length;
-        var obj_l2 = obj_l - 1;
-        console.log(pattern.length, "pattern.length");
-        console.log(e.target.value.length, "e.target.value.length");
-        for (let i = 0; i < pattern.length; i++) {
-          if (obj_l2 == i && pattern.charAt(i + 1) == pattern_ex) {
-            console.log(obj_l2, "obj_l2");
-            // console.log(pattern.charAt(i + 1), "pattern.charAt(i + 1)");
-            returnText += e.target.value + pattern_ex;
-            e.target.value = returnText;
-          }
-        }
-      }
-    }
+        e.target.value = e.target.value.split("").join("-");
+      } else e.target.value = null;
+    } else if (e.target.value.length === 5) {
+      if (/^([HLNO]\d\d\d[A-Z])$/.test(e.target.value)) {
+        e.target.value =
+          e.target.value.slice(0, 1) +
+          "-" +
+          e.target.value.slice(1, e.target.value.length - 1) +
+          "-" +
+          e.target.value.slice(e.target.value.length - 1);
+      } else e.target.value = null;
+    } else if (e.target.value.length === 8) {
+      if (/^([G]\d\d\d[.]\d\d[A-Z])$/.test(e.target.value)) {
+        e.target.value =
+          e.target.value.slice(0, 1) +
+          "-" +
+          e.target.value.slice(1, e.target.value.length - 1) +
+          "-" +
+          e.target.value.slice(e.target.value.length - 1);
+      } else e.target.value = null;
+    } else if (e.target.value.length === 10) {
+      if (/^([ABD]\d\d\d\d\d\d\d\d[A-Z])$/.test(e.target.value)) {
+        e.target.value =
+          e.target.value.slice(0, 1) +
+          "-" +
+          e.target.value.slice(1, e.target.value.length - 1) +
+          "-" +
+          e.target.value.slice(e.target.value.length - 1);
+      } else e.target.value = null;
+    } else e.target.value = null;
+    onChange(e.target.value);
   }
 
   return (
     <>
       <div className={styles.div}>
         <label className={styles.label}>{label}</label>
-
         <Input
           id="strid"
           defaultValue=""
