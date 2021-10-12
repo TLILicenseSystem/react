a8b3dd214920ecec1d3a339652cbbab9b7216e42
awanee.kru <awanee.kru@VDLNIT088.thailife.hq>
2021-10-12 13:23:33 +0700

    buddhist year

--- src/pages/ExamSchedule/EditSchedule.js
+++ src/pages/ExamSchedule/EditSchedule.js
@@ -177,9 +177,8 @@ let EditSchedule = (props) => {
           applyCloseDate: dayjs(data.applyCloseDate).format("YYYY-MM-DD"),
           openStatus: "N",
           receiveDate: dayjs(data.receiveDate).format("YYYY-MM-DD"),
-          receiveTime: dayjs(data.receiveTime, "HH:mm").format("HH:mm"),
+          receiveTime: moment(data.receiveTime, "HH:mm").format("HH:mm"),
           updateUserCode: userModify,
-          lastUpdate: dayjs(new Date()).format("YYYY-MM-DD HH:mm:ss"),
         };
 
         let response = await updateExamSchedule(examSchedule);
@@ -204,9 +203,8 @@ let EditSchedule = (props) => {
           applyCloseDate: dayjs(data.applyCloseDate).format("YYYY-MM-DD"),
           openStatus: "N",
           receiveDate: dayjs(data.receiveDate).format("YYYY-MM-DD"),
-          receiveTime: dayjs(data.receiveTime, "HH:mm").format("HH:mm"),
+          receiveTime: moment(data.receiveTime, "HH:mm").format("HH:mm"),
           updateUserCode: userModify,
-          lastUpdate: dayjs(new Date()).format("YYYY-MM-DD HH:mm:ss"),
         };
         let response = await addExamSchedule(examSchedule);
         if (response !== "error") {
